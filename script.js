173c3d588ee33cdaddd9cafcfab5a3a788d07458
A3a3el666 <maxiboy.2002@mail.ru>
2023-06-13 14:14:45 +0300

    Task1update2

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 const DATA = [
     {
-        quastion:'How many planets are in the solar system?',
+        question:'How many planets are in the solar system?',
+        numOfTrueAnswers: 1,
         answers:[
             {
                 id:'0',
@@ -24,7 +25,8 @@ const DATA = [
         ]
     },
     {
-        quastion:'What is the freezing point of water?',
+        question:'What is the freezing point of water?',
+        numOfTrueAnswers: 1,
         answers:[
             {
                 id:'3',
@@ -47,7 +49,8 @@ const DATA = [
         ]
     },
     {
-        quastion:'What is the capital of Canada?',
+        question:'What is the capital of Canada?',
+        numOfTrueAnswers: 1,
         answers:[
             {
                 id:'6',
@@ -70,7 +73,8 @@ const DATA = [
         ]
     },
     {
-        quastion:'What is the longest river in the world?',
+        question:'What is the longest rivers in the world?',
+        numOfTrueAnswers: 2,
         answers:[
             {
                 id:'9',
@@ -81,7 +85,7 @@ const DATA = [
             {
                 id:'10',
                 value: 'Amazon',
-                correct: false,
+                correct: true,
                 
             },
             {
@@ -103,18 +107,20 @@ const DATA = [
 
 
 let count = 0;
+let nOfTrueAnswers = 0;
+let numOfChoosenAnswers = 0;
 
 const quiz = document.getElementById('quiz');
-const numOfQuastion = document.getElementById('numOfQuastion');
-const quastions = document.getElementById('quastions');
+const numOfQuestion = document.getElementById('numOfQuestion');
+const questions = document.getElementById('questions');
 const results = document.getElementById('results');
 const controlButtonNext = document.getElementById('controlButton-next');
 const controlButtonRestart = document.getElementById('contolButton-restart');
 
-const renderQuastions = (index) => {
-    renderuNumOfQuastion(index+1);
+const renderQuestions = (index) => {
+    renderuNumOfQuestion(index+1);
 
-    quastions.dataset.currentStep = index;
+    questions.dataset.currentStep = index;
 
     const giveId = (answerIndex) => {
         let corectless = answerIndex;
@@ -129,67 +135,78 @@ const renderQuastions = (index) => {
     .map((answer) =>`<button class='answerClick' id= ${giveId(answer.correct)} name="${index} value=${answer.id}">${answer.value}</button>`)
     .join('');
 
-    quastions.innerHTML = `
-    <div class="quizQuastions-item">
-        <div class="quizQuastions-item-quastion">${DATA[index].quastion}</div>
-        <ul class="quizQuastions-item-answers">${renderAnswers()}</ul>
+    questions.innerHTML = `
+    <div class="quizQuestions-item">
+        <div class="quizQuestions-item-question">${DATA[index].question}</div>
+        <div class = "quizQuestion-item-numOfAnswers">Количество правильных ответов в вопросе: ${DATA[index].numOfTrueAnswers}</div>
+        <ul class="quizQuestions-item-answers">${renderAnswers()}</ul>
     </div>
     `;
+    nOfTrueAnswers = DATA[index].numOfTrueAnswers;
 };
 
 
-const renderuNumOfQuastion = (currentQuastion) => {
-    numOfQuastion.innerHTML = `Вопрос ${currentQuastion}/${DATA.length}`   
+const renderuNumOfQuestion = (currentQuestion) => {
+    numOfQuestion.innerHTML = `Вопрос ${currentQuestion}/${DATA.length}`   
 };
 
 quiz.addEventListener('click',(event) =>{
 
+
     if(event.target.classList.contains('answerClick')){
+        
+        numOfChoosenAnswers++;
 
         if(event.target.id === 'correctAnswer'){
-            count++;
             event.target.id = 'correct';
-            quastions.classList.add('disabled');
-            controlButtonNext.disabled = false;
+            if(nOfTrueAnswers === numOfChoosenAnswers){
+                count++;
+                questions.classList.add('disabled');
+                controlButtonNext.disabled = false;
+                numOfChoosenAnswers = 0;
+            }
         } else if(event.target.id === 'wrongAnswer'){
             event.target.id = 'wrong';
-            quastions.classList.add('disabled');
-            controlButtonNext.disabled = false;
+            if(nOfTrueAnswers === numOfChoosenAnswers){
+                questions.classList.add('disabled');
+                controlButtonNext.disabled = false;
+                numOfChoosenAnswers = 0;
+            }
         }
     }
 
     if(event.target.classList.contains('controlButton-next')){
-        let nextQuiation = Number(quastions.dataset.currentStep)+1;
+        let nextQuiation = Number(questions.dataset.currentStep)+1;
         
         if(DATA.length === nextQuiation){
             renderResults(count);
-            quastions.classList.remove('disabled');
+            questions.classList.remove('disabled');
         } else {
-            renderQuastions(nextQuiation);
-            quastions.classList.remove('disabled');
+            renderQuestions(nextQuiation);
+            questions.classList.remove('disabled');
         }
         
         controlButtonNext.disabled = true;
     }
 
     if(event.target.classList.contains('contolButton-restart')){
-        numOfQuastion.classList.remove('nonVisible');
-        quastions.classList.remove('nonVisible');
+        numOfQuestion.classList.remove('nonVisible');
+        questions.classList.remove('nonVisible');
         controlButtonNext.classList.remove('nonVisible');
         controlButtonRestart.classList.remove('visible');
         results.classList.remove('visible');
         count = 0;
-        renderQuastions(0);
+        renderQuestions(0);
     }
 });
 
 const renderResults = (count) => {
-    numOfQuastion.classList.add('nonVisible');
-    quastions.classList.add('nonVisible');
+    numOfQuestion.classList.add('nonVisible');
+    questions.classList.add('nonVisible');
     controlButtonNext.classList.add('nonVisible');
     controlButtonRestart.classList.add('visible');
     results.classList.add('visible');
     results.innerHTML = `Вы решили ${count} из ${DATA.length} вопросов.`
 };
 
-renderQuastions(0);
+renderQuestions(0);
