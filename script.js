9f635102aea86b4700b5082229619cddd8f8d55d
A3a3el666 <maxiboy.2002@mail.ru>
2023-06-18 15:15:30 +0300

    Task1update7

--- script.js
+++ script.js
@@ -104,11 +104,16 @@ const DATA = [
     },
 ];
 
-let sumTime = 0;
-let time = 10;
-let count = 0;
-let nOfTrueAnswers = 0;
+let sumTime;
+let time;
+let count;
+let numberOfTrueAnswers = 0;
 let numOfSelectedAnswers = 0;
+let checkCorrect = 0;
+let nextQuestion;
+let theme;
+
+const LS = localStorage;
 
 const quiz = document.getElementById('quiz');
 const timer = document.getElementById('timer');
@@ -121,17 +126,35 @@ const controlButtonRestart = document.getElementById('controlButton-restart');
 
 
 const updateTimer = () => {
-    time = time < 10 ? "0" + time : time;
-    timer.innerHTML = `${time}`;
-    time--;
-    if(time < 0){
-        controlButtonNext.disabled = false;
-        controlButtonNext.click();
+    nextQuestion = Number(questions.dataset.currentStep)+1;
+
+    if(JSON.parse(LS.getItem('CurrentState')) <= DATA.length){
+        time = time < 10 ? "0" + time : time;
+        timer.innerHTML = `${time}`;
+        time--;
+        LS.setItem('time',JSON.stringify(time));
+    }
+
+    if(time < 0 && JSON.parse(LS.getItem('CurrentState')) != (DATA.length + 1)){
+        if(DATA.length === nextQuestion){
+            sumTime += (10 - time);
+            LS.setItem('SumTime', JSON.stringify(sumTime));
+            renderResults(count);
+            questions.classList.remove('disabled');
+        } else {
+            sumTime += (10 - time);
+            LS.setItem('SumTime', JSON.stringify(sumTime));
+            time = 10;
+            renderQuestions(nextQuestion);
+            questions.classList.remove('disabled');
+        }
     }
 };
 
 const renderQuestions = (index) => {
     renderNumOfQuestion(index+1);
+
+    LS.setItem('CurrentState',JSON.stringify(index));
 
     questions.dataset.currentStep = index;
 
@@ -146,7 +169,7 @@ const renderQuestions = (index) => {
         <ul class="quizQuestions-item-answers">${renderAnswers()}</ul>
     </div>
     `;
-    nOfTrueAnswers = DATA[index].numOfTrueAnswers;
+    numberOfTrueAnswers = DATA[index].numOfTrueAnswers;
 };
 
 
@@ -158,20 +181,19 @@ quiz.addEventListener('click',(event) =>{
 
 
     if(event.target.classList.contains('answerClick')){
-        
         numOfSelectedAnswers++;
 
         if(event.target.id === 'correctAnswer'){
             event.target.id = 'correct';
-            if(nOfTrueAnswers === numOfSelectedAnswers){
-                count++;
+            checkCorrect++;
+            if(numberOfTrueAnswers === numOfSelectedAnswers){
                 questions.classList.add('disabled');
                 controlButtonNext.disabled = false;
                 numOfSelectedAnswers = 0;
             }
         } else if(event.target.id === 'wrongAnswer'){
             event.target.id = 'wrong';
-            if(nOfTrueAnswers === numOfSelectedAnswers){
+            if(numberOfTrueAnswers === numOfSelectedAnswers){
                 questions.classList.add('disabled');
                 controlButtonNext.disabled = false;
                 numOfSelectedAnswers = 0;
@@ -180,19 +202,27 @@ quiz.addEventListener('click',(event) =>{
     }
 
     if(event.target.classList.contains('controlButton-next')){
-    let nextQuestion = Number(questions.dataset.currentStep)+1;
         
+        if(checkCorrect/numberOfTrueAnswers === 1){
+            count++;
+            checkCorrect = 0;
+        }
+
+        LS.setItem('Count',JSON.stringify(count));
+
         if(DATA.length === nextQuestion){
             sumTime += (10 - time);
+            LS.setItem('SumTime', JSON.stringify(sumTime));
             renderResults(count);
             questions.classList.remove('disabled');
         } else {
             sumTime += (10 - time);
+            LS.setItem('SumTime', JSON.stringify(sumTime));
             time = 10;
             renderQuestions(nextQuestion);
             questions.classList.remove('disabled');
         }
-        
+
         controlButtonNext.disabled = true;
     }
 
@@ -205,29 +235,49 @@ quiz.addEventListener('click',(event) =>{
         controlButtonRestart.classList.remove('visible');
         results.classList.remove('visible');
         count = 0;
+        LS.setItem('Count', JSON.stringify(count));
+        time = 10;
         sumTime = 0;
         renderQuestions(0);
     }
     if(event.target.classList.contains('themeSwitchButton')){
-        if(quiz.classList.contains('darkTheme')){
+        if(theme === 1){
             document.body.style.background = 'rgb(135, 220, 220)';
             quiz.classList.remove('darkTheme');
             themeSwitchButton.classList.remove('btnDarkTheme');
             timer.classList.remove('btnDarkTheme');
             controlButtonNext.classList.remove('btnDarkTheme');
             controlButtonRestart.classList.remove('btnDarkTheme');
+
+            theme = 0;
+            LS.setItem('CurrentTheme',JSON.stringify(theme));
         }else{
+            document.body.style.background = '#293659';
+            quiz.classList.add('darkTheme');
+            themeSwitchButton.classList.add('btnDarkTheme');
+            timer.classList.add('btnDarkTheme');
+            controlButtonNext.classList.add('btnDarkTheme');
+            controlButtonRestart.classList.add('btnDarkTheme');
+
+            theme = 1;
+            LS.setItem('CurrentTheme',JSON.stringify(theme));
+        }
+    }
+});
+
+const themeSave = (theme) => {
+    if(theme === 1){
         document.body.style.background = '#293659';
         quiz.classList.add('darkTheme');
         themeSwitchButton.classList.add('btnDarkTheme');
         timer.classList.add('btnDarkTheme');
         controlButtonNext.classList.add('btnDarkTheme');
         controlButtonRestart.classList.add('btnDarkTheme');
-        }
     }
-});
+};
 
 const renderResults = (count) => {
+
     timer.classList.add('nonVisible');
     themeSwitchButton.classList.add('nonVisible');
     numOfQuestion.classList.add('nonVisible');
@@ -236,9 +286,27 @@ const renderResults = (count) => {
     controlButtonRestart.classList.add('visible');
     results.classList.add('visible');
 
-    results.innerHTML = `<p class="result">Вы решили ${count} из ${DATA.length} вопросов за ${sumTime = sumTime > 40 ? sumTime-DATA.length : sumTime} секунд.</p>`
+    let finishTime = JSON.parse(LS.getItem('SumTime'));
+    count = JSON.parse(LS.getItem('Count'));
+
+    results.innerHTML = `<p class="result">Вы решили ${count} из ${DATA.length} вопросов за ${finishTime = finishTime > 40 ? finishTime-DATA.length : finishTime} секунд.</p>`
+
+    LS.setItem('CurrentState',JSON.stringify(DATA.length+1));
 };
 
-renderQuestions(0);
+if(JSON.parse(LS.getItem('CurrentState')) === null){
+    renderQuestions(0);
+}else if(JSON.parse(LS.getItem('CurrentState')) > DATA.length){
+    renderResults(count);
+}else{
+    renderQuestions(JSON.parse(LS.getItem('CurrentState')));
+}
+
+JSON.parse(LS.getItem('time')) === null ? time = 10 : time = JSON.parse(LS.getItem('time'));
+JSON.parse(LS.getItem('CurrentState')) === null || JSON.parse(LS.getItem('CurrentState')) === 0 ? sumTime = 0 : sumTime = JSON.parse(LS.getItem('SumTime'));
+JSON.parse(LS.getItem('Count')) === null || JSON.parse(LS.getItem('CurrentState')) === 0 ? count = 0 : count = JSON.parse(LS.getItem('Count'));
+JSON.parse(LS.getItem('CurrentTheme') === null) ? theme = 0 : theme = JSON.parse(LS.getItem('CurrentTheme'));
+themeSave(theme);
+
 
 setInterval(updateTimer,1000);
